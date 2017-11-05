0816697ee99403bd71eb48848ff62a17eb8aba10
Alexey Bondarenko <alexeybondarenko@me.com>
2017-11-06 00:53:01 +0200

    Header. Descop and mobile

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,23 @@
 //=include lib/slick.js
 
 console.log('App is running')
+
+$(document).ready(() => {
+  $('.hamb').on('click', function() {
+    $(this).toggleClass('hamb_active')
+    $('.nav').toggleClass('nav_active')
+    $('body').toggleClass('disable-scroll')
+  })
+
+  $('.nav-submenu-item').on('click', function (e) {
+    e.stopPropagation()
+  })
+  $('.nav__item_with-submenu > a').on('click', function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    $(this).parent('.nav__item_with-submenu').toggleClass('nav__item_active')
+  })
+
+
+  $('.hamb').click()
+})
