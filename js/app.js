1fb1f7ab72eca1d25f3ab3b2cfc48e95cb8d4528
Alexey Bondarenko <alexeybondarenko@me.com>
2017-11-06 02:56:30 +0200

    Update 2017-11-06T00:56:26.644Z

--- js/app.js
+++ js/app.js
@@ -2956,3 +2956,23 @@ return new Za.prototype.init(a,b,c,d,e)}m.Tween=Za,Za.prototype={constructor:Za,
 
 
 console.log('App is running')
+
+$(document).ready(() => {
+  $('.hamb').on('click', function() {
+    $(this).toggleClass('hamb_active')
+    $('.nav').toggleClass('nav_active')
+    $(document.documentElement).toggleClass('disable-scroll')
+  })
+
+  $('.nav-submenu-item').on('click', function (e) {
+    e.stopPropagation()
+  })
+  $('.nav__item_with-submenu > a').on('click', function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    $(this).parent('.nav__item_with-submenu').toggleClass('nav__item_active')
+  })
+
+
+  // $('.hamb').click()
+})
